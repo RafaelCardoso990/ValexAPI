c9d3f891f3bfa447dab77f66bae1e417779abbdf
RafaelCardoso990 <rafaelcardoso990@gmail.com>
2022-07-08 01:51:29 -0300

    configured project

---
+++ config/db.ts
@@ -0,0 +1,14 @@
+import pg from "pg"
+import dotenv from "dotenv"
+
+dotenv.config();
+
+const {Pool} = pg
+
+const configDataBase = {connectionString: process.env.DATABASE_URL}
+
+console.log("postgres connected")
+
+const db = new Pool(configDataBase)
+
+export default db
---
+++ index.ts
@@ -0,0 +1,13 @@
+import express from "express"
+import cors from "cors"
+
+const app = express();
+
+app.use(cors())
+app.use(express.json())
+
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log("Server is running on port " + port);
+});
